df3e1c49943f4e1e3eec4f743b11ea75d28d49bb
Ayush Kumar <124272617+Ayush247874@users.noreply.github.com>
2024-11-29 07:00:22 +0000

    container config done

---
+++ 01_basic/variable.js
@@ -0,0 +1,63 @@
+// var name="Ayush";
+// var name="Kumar";
+
+// console.log(name);
+
+// let name="Suuny";
+// let name="Raj";
+
+// console.log(name);
+
+// if (true) {
+//   var x = 5;
+// }
+// console.log(x);
+
+
+// if (true) {
+  // let y = 10;
+// }
+// console.log(y);
+
+// console.log(a); // undefined, because `a` is hoisted but not assigned yet
+// var a = 10;
+// console.log(b); // ReferenceError: Cannot access 'b' before initialization
+// let b = 20;
+
+
+
+const accountId=144553;
+let accountEmail="ayush@gmail.com";
+var accountPassword="12345";
+accountCity="Patna";
+
+
+// console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
+
+// accountId=23423434;
+ accountEmail="aysu77789";
+ accountPassword="8787";
+accountCity="Mohali";
+let accounStaste;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accounStaste]);
+
+
+
+// let num="34";
+// let num="45";
+
+// console.log(num);
+
+
+
+
+
+
+/**
+ * prefer not t
+ * 
+ * 
+ */
